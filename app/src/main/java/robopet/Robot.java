e646edb719ca2bd50104df2ecb909b795d75f852
Paul Loch <134151519+paulloch@users.noreply.github.com>
2023-08-04 00:19:33 -0400

    initial upload
    Dog.java from Azalea
    Cat.java from Shaun
    The rest me
    still waiting Hashmaps from Johnathan

---
+++ app/src/main/java/robopet/Robot.java
@@ -0,0 +1,31 @@
+public class Robot extends Pet {
+    
+    private String name;
+    private String description;
+    private int hunger;
+    private int thirst;
+    private int mange;
+    private int boredom;
+    private int dirtyHabitat;
+    private int days;
+    private boolean canine;
+    
+    public Robot (String name, String description, int health, int thirst, int days){
+
+    }
+
+    
+    public void chargeRobot(){
+
+
+
+    }
+
+    public void oilRobot(){
+
+
+
+    }
+
+
+}
