2f395e654334faac4a9b088173e44dfb2b97fc7f
Paul Loch <134151519+paulloch@users.noreply.github.com>
2023-08-12 22:53:28 -0400

    Add files via upload

--- app/src/main/java/robopet/Shelter.java
+++ app/src/main/java/robopet/Shelter.java
@@ -18,31 +18,31 @@ public class Shelter {
 	boolean canine;
 	String pet;
 
-	//  16 pets for the shelter
+	// 16 pets for the shelter
 	public void existingPets() {
-		
+
 		Dog dog1 = new Dog("", "", 18, 22, 17, 15, 24, 20, true, true);
 		dog1.setName("Snoopy");
 		dog1.setDescription("White Beagle");
 		dog1.setDays(20);
 		shelterMap.put("Snoopy", dog1);
-		
+
 		Dog dog2 = new Dog("", "", 30, 32, 37, 25, 38, 29, true, true);
 		dog2.setName("Scooby");
 		dog2.setDescription("Hound");
-		dog2.setDays(29);		
+		dog2.setDays(29);
 		shelterMap.put("Scooby", dog2);
-		
+
 		Dog dog3 = new Dog("", "", 26, 25, 33, 18, 14, 12, true, true);
 		dog3.setName("Pongo");
 		dog3.setDescription("Dalmatian");
 		dog3.setDays(12);
 		shelterMap.put("Pongo", dog3);
-		
+
 		Dog dog4 = new Dog("", "", 34, 12, 19, 39, 27, 30, true, true);
 		dog4.setName("Hooch");
 		dog4.setDescription("Mastiff");
-		dog4.setDays(30);		
+		dog4.setDays(30);
 		shelterMap.put("Hooch", dog4);
 
 		Cat cat1 = new Cat("", "", 11, 14, 16, 22, 12, 18, false, true);
@@ -50,13 +50,13 @@ public class Shelter {
 		cat1.setDescription("Orange Tabby");
 		cat1.setDays(18);
 		shelterMap.put("Garfield", cat1);
-		
+
 		Cat cat2 = new Cat("", "", 23, 25, 36, 12, 33, 17, false, true);
 		cat2.setName("Tom");
 		cat2.setDescription("Alley Cat");
 		cat2.setDays(17);
 		shelterMap.put("Tom", cat2);
-		
+
 		Cat cat3 = new Cat("", "", 18, 33, 45, 25, 19, 22, false, true);
 		cat3.setName("Sassy");
 		cat3.setDescription("Black Tabby");
@@ -69,53 +69,53 @@ public class Shelter {
 		cat4.setDays(33);
 		shelterMap.put("Mr. Bigglesworth", cat4);
 
-		Robot robot1 = new Robot("","",80,100,28,true,false);
+		Robot robot1 = new Robot("", "", 80, 100, 28, true, false);
 		robot1.setName("Bruiser");
 		robot1.setDescription("Chihuahua");
 		robot1.setDays(28);
-		shelterMap.put("Bruiser",robot1);
+		shelterMap.put("Bruiser", robot1);
 
-		Robot robot2 = new Robot("","",100,10,44,true,false);
+		Robot robot2 = new Robot("", "", 100, 10, 44, true, false);
 		robot2.setName("Lassie");
 		robot2.setDescription("Collie");
 		robot2.setDays(44);
 
-		shelterMap.put("Lassie",robot2);
-		Robot robot3 = new Robot("","",70,100,23,true,false);
+		shelterMap.put("Lassie", robot2);
+		Robot robot3 = new Robot("", "", 70, 100, 23, true, false);
 		robot3.setName("Clifford");
 		robot3.setDescription("Big Red Dog");
 		robot3.setDays(23);
-		shelterMap.put("Clifford",robot3);
+		shelterMap.put("Clifford", robot3);
 
-		Robot robot4 = new Robot("","",90,100,7,true,false);
+		Robot robot4 = new Robot("", "", 90, 100, 7, true, false);
 		robot4.setName("Air Bud");
 		robot4.setDescription("Golden Retriever");
 		robot4.setDays(7);
-		shelterMap.put("AirBud",robot4);
+		shelterMap.put("AirBud", robot4);
 
-		Robot robot5 = new Robot("","",80,100,18,false,false);
+		Robot robot5 = new Robot("", "", 80, 100, 18, false, false);
 		robot5.setName("Salem");
 		robot5.setDescription("Black Witch Cat");
 		robot5.setDays(18);
-		shelterMap.put("Salem",robot5);		
+		shelterMap.put("Salem", robot5);
 
-		Robot robot6 = new Robot("","",80,100,24,false,false);
+		Robot robot6 = new Robot("", "", 80, 100, 24, false, false);
 		robot6.setName("Morris");
 		robot6.setDescription("Commercial Cat");
 		robot6.setDays(21);
-		shelterMap.put("Morris",robot6);
+		shelterMap.put("Morris", robot6);
 
-		Robot robot7 = new Robot("","",90,100,34,false,false);
+		Robot robot7 = new Robot("", "", 90, 100, 34, false, false);
 		robot7.setName("Grumpy Cat");
 		robot7.setDescription("Tardar");
 		robot7.setDays(33);
-		shelterMap.put("Grumpy Cat",robot7);		
+		shelterMap.put("Grumpy Cat", robot7);
 
-		Robot robot8 = new Robot("","",90,10,14,false,false);
+		Robot robot8 = new Robot("", "", 90, 10, 14, false, false);
 		robot8.setName("Mr. Jinx");
 		robot8.setDescription("Himalayan");
 		robot8.setDays(14);
-		shelterMap.put("Mr.Jinx",robot8);
+		shelterMap.put("Mr.Jinx", robot8);
 
 	}
 
@@ -155,9 +155,11 @@ public class Shelter {
 	}
 
 	public void addOrganicDog() {
-		System.out.println("Enter Pet Name (identical name will not save)");
-		KeyboardInput name = new KeyboardInput();
-		petName = name.getStringValue("");
+		do {
+			System.out.println("Enter Pet Name (identical name will not save)");
+			KeyboardInput name = new KeyboardInput();
+			petName = name.getStringValue("");
+		} while (shelterMap.containsKey(petName) == true);
 		System.out.println();
 		System.out.println("Enter Pet Description");
 		KeyboardInput newDescription = new KeyboardInput();
@@ -173,9 +175,11 @@ public class Shelter {
 	}
 
 	public void addOrganicCat() {
-		System.out.println("Enter Pet Name (identical name will not save)");
-		KeyboardInput name = new KeyboardInput();
-		petName = name.getStringValue("");
+		do {
+			System.out.println("Enter Pet Name (identical name will not save)");
+			KeyboardInput name = new KeyboardInput();
+			petName = name.getStringValue("");
+		} while (shelterMap.containsKey(petName) == true);
 		System.out.println();
 		System.out.println("Enter Pet description");
 		KeyboardInput newDescription = new KeyboardInput();
@@ -190,9 +194,11 @@ public class Shelter {
 	}
 
 	public void addRoboticDog() {
-		System.out.println("Enter Pet Name (identical name will not save)");
-		KeyboardInput name = new KeyboardInput();
-		petName = name.getStringValue("");
+		do {
+			System.out.println("Enter Pet Name (identical name will not save)");
+			KeyboardInput name = new KeyboardInput();
+			petName = name.getStringValue("");
+		} while (shelterMap.containsKey(petName) == true);
 		System.out.println();
 		System.out.println("Enter Pet description");
 		KeyboardInput newDescription = new KeyboardInput();
@@ -207,9 +213,11 @@ public class Shelter {
 	}
 
 	public void addRoboticCat() {
-		System.out.println("Enter Pet Name (identical name will not save)");
-		KeyboardInput name = new KeyboardInput();
-		petName = name.getStringValue("");
+		do {
+			System.out.println("Enter Pet Name (identical name will not save)");
+			KeyboardInput name = new KeyboardInput();
+			petName = name.getStringValue("");
+		} while (shelterMap.containsKey(petName) == true);
 		System.out.println();
 		System.out.println("Enter Pet description");
 		KeyboardInput newDescription = new KeyboardInput();
@@ -337,7 +345,7 @@ public class Shelter {
 							System.out.println("After Charging Pet");
 							entry.getValue().infoPet();
 							break;
-						case 2:												
+						case 2:
 							entry.getValue().tick();
 							entry.getValue().water();
 							System.out.println();
